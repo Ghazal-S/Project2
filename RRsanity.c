485858afc480a3e252804e82da467d7a540484f3
Ghazal-S <45939148+Ghazal-S@users.noreply.github.com>
2019-06-26 13:58:56 +0430

    RRsanity modified

--- RRsanity.c
+++ RRsanity.c
@@ -4,11 +4,11 @@
 #include "user.h"
 
 void
-foo(int pid)
+foo()
 {
   int i;
   for (i=0;i<1000;i++){
-	printf(2, "Child %d prints for the %d time \n",pid,i);
+	printf(2, "Child %d prints for the %d time \n",getpid(),i);
   }
 
 }
@@ -18,20 +18,29 @@ RRsanity(void)
 {
   int wTime;
   int rTime;
+  int pid,k;
   printf(1, "RRsanity\n");
-  for(int i=0;i<10;i++) // loop will run 10 times
-    { 
-        if(fork() == 0) 
-        { 
-	    foo(getpid()); 
-            exit(); 
-        } 
-    } 
-    //for(int i=0;i<10;i++) // loop will run 10 times
-    //wait(NULL); 
+  for ( k = 0; k < 10; k++ ) {
+    pid = fork ();
+    if ( pid < 0 ) {
+      printf(1, "%d failed in fork!\n", getpid());
+      exit();
+    } else if (pid == 0) {
+      // child
+      foo();
+      exit();
+    }
+  }
+
+  for (k = 0; k < 10; k++) {
+    wait();
+  }
+
+  
       
     getPerformanceData(&wTime,&rTime);
     printf(1, "wTime: %d rTime: %d \n",wTime,rTime);
+    exit();
 }
 
 
